f6435e63cdcb98c41f017e1c7dd6f49aa4284840
nguyen.tuan <nguyen.tuan@deskera.com>
2020-04-30 11:44:34 +0800

    commit code from local

--- src/main/java/com/data/dangtuan/SparkExperimentApplication.java
+++ src/main/java/com/data/dangtuan/SparkExperimentApplication.java
@@ -1,4 +1,4 @@
-package com.zhuinden.sparkexperiment;
+package com.data.dangtuan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SparkExperimentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SparkExperimentApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SparkExperimentApplication.class, args);
+  }
 }
---
+++ src/main/java/com/data/dangtuan/Word.java
@@ -0,0 +1,23 @@
+package com.data.dangtuan;
+
+public class Word {
+
+  private String word;
+
+
+  public Word() {
+  }
+
+
+  public Word(String word) {
+    this.word = word;
+  }
+
+  public String getWord() {
+    return word;
+  }
+
+  public void setWord(String word) {
+    this.word = word;
+  }
+}
