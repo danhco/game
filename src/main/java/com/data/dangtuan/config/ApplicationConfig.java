492acf13a4fef9011289ccf6fc93951ebab2f045
nguyen.tuan <nguyen.tuan@deskera.com>
2020-05-25 11:07:37 +0800

    remove unuse

--- src/main/java/com/data/dangtuan/config/ApplicationConfig.java
+++ src/main/java/com/data/dangtuan/config/ApplicationConfig.java
@@ -7,12 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:application.yml")
 public class ApplicationConfig {
 
   @Autowired
